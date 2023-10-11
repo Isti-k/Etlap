59a70cb30258ead16e31867b340af20cf7dab8e6
Isti-k <kiscserkes03@gmail.com>
2023-10-11 11:58:52 +0200

    Etlap

---
+++ kozos_eljaras.py
@@ -0,0 +1,17 @@
+import etlap
+
+import rendeles
+
+#főcim eljaras1
+def focim(jel, db):
+    print(jel * db)
+def szoveg_kiiaras(jel, szoveg, jel2):
+    print(f"{jel} {szoveg:^28} {jel2}")
+
+#Ételek eljaras2
+
+#csillag32 eljara3
+def csillagok(jel, db):
+    print(jel * db)
+def foetelek(szoveg, szam):
+    print(f"{szoveg:<12} {szam:>13}")
